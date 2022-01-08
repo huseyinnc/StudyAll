6b42b9387869627b88a088d108a857c0c70fa76c
Huseyin <huseyinnc_15@hotmail.com>
2022-01-08 03:02:50 +0300

    i made a mistake, when i added the note i get an error, i can't solve right know.

--- app/src/main/java/com/example/studyall/CreateNotesActivity.java
+++ app/src/main/java/com/example/studyall/CreateNotesActivity.java
@@ -4,23 +4,32 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateNotesActivity extends AppCompatActivity {
 
-    private EditText createtitlenote,createcontentnote;
-    private FloatingActionButton savenotefab;
-    private FirebaseAuth mAuth;
-    private FirebaseUser user;
-    private FirebaseFirestore firestore;
-    private Toolbar toolbar;
+    EditText mcreatetitlenote,mcreatecontentnote;
+    FloatingActionButton msavenotefab;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firestore;
+
 
 
     @Override
@@ -28,18 +37,52 @@ public class CreateNotesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_notes);
 
-        createtitlenote = (EditText) findViewById(R.id.createtitlenote);
-        createcontentnote = (EditText) findViewById(R.id.createcontentnote);
-        savenotefab = (FloatingActionButton) findViewById(R.id.savenotefab);
+        mcreatetitlenote = findViewById(R.id.createtitlenote);
+        mcreatecontentnote = findViewById(R.id.createcontentnote);
+        msavenotefab = findViewById(R.id.savenotefab);
 
-        toolbar = (Toolbar) findViewById(R.id.createnotefab);
+        Toolbar toolbar = findViewById(R.id.toolbarofcreatenote);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        mAuth = FirebaseAuth.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
-        user = FirebaseAuth.getInstance().getCurrentUser();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        msavenotefab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = mcreatetitlenote.getText().toString();
+                String content = mcreatecontentnote.getText().toString();
+                if (title.isEmpty() || content.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Both field are require!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //match the notesactivitiy about collection and document
+                    DocumentReference documentReference = firestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").document();
+                    Map<String, Object> notes = new HashMap<>();
+                    notes.put("title",title);
+                    notes.put("content",content);
+
+                    documentReference.set(notes).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Toast.makeText(getApplicationContext(), "Note created succesfully!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(CreateNotesActivity.this,NotesActivity.class));
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), "Failed to create note!", Toast.LENGTH_SHORT).show();
+                            //startActivity(new Intent(CreateNotesActivity.this,NotesActivity.class));
+                        }
+                    });
+
+                }
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/studyall/NotesActivity.java
+++ app/src/main/java/com/example/studyall/NotesActivity.java
@@ -2,28 +2,53 @@ package com.example.studyall;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.service.controls.actions.FloatAction;
+
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class NotesActivity extends AppCompatActivity {
-    private FloatingActionButton createnotefab;
+    FloatingActionButton mcreatenotefab;
+    FirebaseAuth firebaseAuth;
+    RecyclerView recyclerView;
+    StaggeredGridLayoutManager staggeredGridLayoutManager;
+    FirebaseUser firebaseUser;
+    FirebaseFirestore firestore;
+
+    FirestoreRecyclerAdapter<firebasemodel,NoteViewHolder> noteAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
+        firebaseAuth=FirebaseAuth.getInstance();
+        firebaseUser=FirebaseAuth.getInstance().getCurrentUser();
+        firestore=FirebaseFirestore.getInstance();
+
         getSupportActionBar().setTitle("All Notes");
 
-        createnotefab = (FloatingActionButton) findViewById(R.id.createnotefab);
-        createnotefab.setOnClickListener(new View.OnClickListener() {
+
+        mcreatenotefab = findViewById(R.id.createnotefab);
+        mcreatenotefab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
@@ -31,8 +56,52 @@ public class NotesActivity extends AppCompatActivity {
 
             }
         });
+        //match the CreateNotesActivity about collection and document
+        Query query = firestore.collection("notes").document(firebaseUser.getUid()).collection("myNotes").orderBy("title",Query.Direction.ASCENDING);
+
+        FirestoreRecyclerOptions<firebasemodel> allusernotes = new FirestoreRecyclerOptions.Builder<firebasemodel>().setQuery(query,firebasemodel.class).build();
+
+        noteAdapter = new FirestoreRecyclerAdapter<firebasemodel, NoteViewHolder>(allusernotes) {
+            @Override
+            protected void onBindViewHolder(@NonNull NoteViewHolder noteViewHolder, int i, @NonNull firebasemodel firebasemodel) {
+
+                noteViewHolder.notestitle.setText(firebasemodel.getTitle());
+                noteViewHolder.notescontent.setText(firebasemodel.getContent());
+
+
+
+            }
+
+            @NonNull
+            @Override
+            public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notes_layout,parent,false);
+                return new NoteViewHolder(view);
+            }
+        };
+
+        recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        staggeredGridLayoutManager = new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(staggeredGridLayoutManager);
+        recyclerView.setAdapter(noteAdapter);
 
     }
+
+    public static class NoteViewHolder extends RecyclerView.ViewHolder {
+        final TextView notestitle;
+        final TextView notescontent;
+        LinearLayout notes;
+
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            notestitle = itemView.findViewById(R.id.notestittle);
+            notescontent = itemView.findViewById(R.id.notescontent);
+            notes = itemView.findViewById(R.id.notes);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -48,5 +117,21 @@ public class NotesActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        noteAdapter.startListening();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(noteAdapter!=null){
+            noteAdapter.startListening();
+        }
+
     }
 }
