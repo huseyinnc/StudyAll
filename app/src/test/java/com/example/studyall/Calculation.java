cabbb866a602a1a198a7799e1792328e52ce865c
Huseyin <huseyinnc_15@hotmail.com>
2022-01-13 04:01:01 +0300

    i added the notes to real database not firebase and actually i have a another problem about the read data from real database.
    --also i added example unit test

---
+++ app/src/test/java/com/example/studyall/Calculation.java
@@ -0,0 +1,26 @@
+package com.example.studyall;
+
+
+public class Calculation {
+
+    public double addTwoNumbers(double a, double b) {
+        return a + b;
+    }
+
+    public double subtractTwoNumbers(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplyTwoNumbers(double a, double b) {
+        return a * b;
+    }
+
+    public double divideTwoNumbers(double a, double b) {
+        return a / b;
+    }
+
+    public double calculatePercentage(double a) {
+        return a / 100;
+    }
+
+}
--- app/src/test/java/com/example/studyall/ExampleUnitTest.java
+++ app/src/test/java/com/example/studyall/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import android.icu.util.IslamicCalendar;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,4 +16,27 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+
+    @Test
+    public void subtraction_isCorrect() {
+        assertEquals(5,new Calculation().subtractTwoNumbers(8,3),0);
+    }
+
+    @Test
+    public void multiplication_isCorrect() {
+        assertEquals(24, new Calculation().multiplyTwoNumbers(8,3),0);
+    }
+
+    @Test
+    public void division_isCorrect() {
+        assertEquals(3, new Calculation().divideTwoNumbers(9,3),0);
+    }
+
+    @Test
+    public void percentage_isCorrect() {
+        assertEquals(0.03, new Calculation().calculatePercentage(3),0);
+    }
+
+
 }
